3770ac29a3a38accc847dd2173d065e4589f59b9
phantom08266 <phantom08266@gmail.com>
2021-03-10 00:28:12 +0900

    Fix 큐레이터 테이블 unique index 설정

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,6 +1,9 @@
 create table Curator(
-    email    varchar(64) not null primary key,
-    username varchar(64) not null,
+    id int(10) not null auto_increment,
+    email varchar(64) not null,
+    name varchar(64) not null,
     password varchar(64) not null,
-    salt     varchar(64) not null
+    salt varchar(64) not null,
+    primary key (id),
+    unique index idx_email (email)
 );
