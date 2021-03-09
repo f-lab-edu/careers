dc7a4eaa29a1ecb967e1f38aad6c77466acc2d48
phantom08266 <phantom08266@gmail.com>
2021-03-10 00:48:00 +0900

    Fix 회원가입 http method 변경
    put -> post

--- src/main/java/com/dev/careers/controller/CuratorController.java
+++ src/main/java/com/dev/careers/controller/CuratorController.java
@@ -2,16 +2,15 @@ package com.dev.careers.controller;
 
 import com.dev.careers.model.Curator;
 import com.dev.careers.service.CuratorService;
+import java.util.List;
+import java.util.Optional;
+import javax.validation.Valid;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 public class CuratorController {
@@ -22,7 +21,7 @@ public class CuratorController {
         this.curatorService = curatorService;
     }
 
-    @PutMapping("/curators/join")
+    @PostMapping("/curators/join")
     public String putMember(@Valid @ModelAttribute Curator curator, BindingResult bindingResult) throws Exception {
         if (bindingResult.hasErrors()){
             Optional<ObjectError> objectError = bindingResult.getAllErrors().stream().findFirst();
--- src/test/java/com/dev/careers/controller/CuratorControllerTest.java
+++ src/test/java/com/dev/careers/controller/CuratorControllerTest.java
@@ -1,5 +1,10 @@
 package com.dev.careers.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,11 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Transactional
@@ -30,7 +30,7 @@ class CuratorControllerTest {
     @Test
     @DisplayName("정상적인 회원가입")
     public void joinCurator() throws Exception {
-        mockMvc.perform(put("/curators/join")
+        mockMvc.perform(post("/curators/join")
         .param("email","test@google.com")
         .param("name", "홍길동")
         .param("password", "test123!@"))
@@ -41,7 +41,7 @@ class CuratorControllerTest {
     @Test
     @DisplayName("잘못된 이메일 형식 요청")
     public void violationEmail() throws Exception {
-        mockMvc.perform(put("/curators/join")
+        mockMvc.perform(post("/curators/join")
         .param("email", "test123.com")
         .param("name", "홍길동")
         .param("password", "test123!@"))
@@ -53,7 +53,7 @@ class CuratorControllerTest {
     @Test
     @DisplayName("잘못된 비밀번호 형식 요청")
     public void violationPassword() throws Exception {
-        mockMvc.perform(put("/curators/join")
+        mockMvc.perform(post("/curators/join")
                 .param("email", "test@google.com")
                 .param("name", "홍길동")
                 .param("password", "123"))
