a0ef85cb880c43321ae02206264eed1b6631169d
phantom08266 <phantom08266@gmail.com>
2021-03-10 23:18:56 +0900

    Fix model코드 가독성 처리

--- src/main/java/com/dev/careers/model/Curator.java
+++ src/main/java/com/dev/careers/model/Curator.java
@@ -12,16 +12,20 @@ import javax.validation.constraints.Pattern;
 public class Curator {
     @Nullable
     private int id;
+
     @NonNull
     @Email(message = "Email Format Violation")
     private String email;
+    
     @NonNull
     private String name;
-    @NonNull
+
     //최소 8자리에 숫자, 문자, 특수문자 각각 1개 이상 포함
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,}$",
             message = "Password Format Violation")
+    @NonNull
     private String password;
+
     @Nullable
     private String salt;
 }
