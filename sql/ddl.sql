dfda0e3d02670a22cc4d6ea47d6e825091d460b6
phantom08266 <phantom08266@gmail.com>
2021-04-02 02:25:25 +0900

    Add profile 테이블 추가
    https://github.com/f-lab-edu/careers/issues/8

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -4,6 +4,16 @@ create table Curator(
     name varchar(64) not null,
     password varchar(64) not null,
     salt varchar(64) not null,
-    primary key (id),
+    primary key (id, name), -- Profile 테이블에서 foreign key로 사용하기 위해 name도 pk설정
     unique index idx_email (email)
+);
+
+create table Profile(
+    profileId int(10) not null auto_increment,
+    curatorId int(10) not null,
+    curatorName varchar(64) not null,
+    title varchar(64),
+    introduction varchar(64),
+    primary key (profileId),
+    foreign key (curatorId, curatorName) references curator (id, name)
 );
