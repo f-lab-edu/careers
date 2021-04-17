987d12a624a5d7bf28537cafb4f1303eab2fb5b6
ByeongJun <dnjwm8612@gmail.com>
2021-04-17 23:39:12 +0900

    Fix 회원가입 기능 반환 변수 이동 - 다른 클래스에 사용할 수 있도록 별도에 클래스 생성 후 이동 - 변수 이동에 따른 반환 값 부분 코드 변경

--- src/main/java/com/dev/careers/controller/CuratorController.java
+++ src/main/java/com/dev/careers/controller/CuratorController.java
@@ -6,7 +6,6 @@ import java.net.URISyntaxException;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,23 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CuratorController {
-    final static ResponseEntity<Void> BAD_REQUEST = new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
-    final static ResponseEntity<Void> CREATE = new ResponseEntity<Void>(HttpStatus.CREATED);
 
     @Autowired
     private CuratorService curatorService;
 
-
     @PostMapping("/curators")
-    public ResponseEntity<Void> create(@Valid @RequestBody Curator curator,
+    public HttpStatus create(@Valid @RequestBody Curator curator,
         BindingResult bindingResult) throws URISyntaxException {
         if(bindingResult.hasErrors()) {
-            return BAD_REQUEST;
+            return HttpStatus.BAD_REQUEST;
         }
 
         curatorService.addCurator(curator);
 
-        return CREATE;
+        return HttpStatus.CREATED;
     }
 
     @PostMapping("/curators/confirm-Email/{email}")
---
+++ src/main/java/com/dev/careers/domain/HttpStatus.java
@@ -0,0 +1,10 @@
+package com.dev.careers.domain;
+
+import org.springframework.http.ResponseEntity;
+
+public class HttpStatus {
+    public final static ResponseEntity<Void> BAD_REQUEST = new ResponseEntity<Void>(
+        org.springframework.http.HttpStatus.BAD_REQUEST);
+    public final static ResponseEntity<Void> CREATE = new ResponseEntity<Void>(
+        org.springframework.http.HttpStatus.CREATED);
+}
