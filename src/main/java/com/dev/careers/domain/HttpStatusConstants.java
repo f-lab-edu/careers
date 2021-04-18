ba2adbb206ee2156f130dfd1a26c733e05757dc7
ByeongJun <dnjwm8612@gmail.com>
2021-04-18 16:15:10 +0900

    Fix HttpStatus 클래스 이름 변경 - 스프링에서 제공하는 HttpStatus Enum클래스와 충돌 방지를 위해 현재 이름에 Constants를 추가

--- src/main/java/com/dev/careers/domain/HttpStatusConstants.java
+++ src/main/java/com/dev/careers/domain/HttpStatusConstants.java
@@ -2,7 +2,7 @@ package com.dev.careers.domain;
 
 import org.springframework.http.ResponseEntity;
 
-public class HttpStatus {
+public class HttpStatusConstants {
     public final static ResponseEntity<Void> BAD_REQUEST = new ResponseEntity<Void>(
         org.springframework.http.HttpStatus.BAD_REQUEST);
     public final static ResponseEntity<Void> CREATE = new ResponseEntity<Void>(
