5be9debe98344ecfc29181a1771733ddcd6c9125
ByeongJun <dnjwm8612@gmail.com>
2021-08-04 01:15:33 +0900

    Add 투표 서비스 테스트 클래스 예외 관련 단위 테스트 추가 (#38) - 목록 조회, 생성, 삭제 처리 과정에서 발생하는 예외 처리 과정에 대한 단위 테스트 추가

--- src/test/java/com/dev/careers/service/VotingServiceTest.java
+++ src/test/java/com/dev/careers/service/VotingServiceTest.java
@@ -2,28 +2,41 @@ package com.dev.careers.service;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.will;
 import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import com.dev.careers.mapper.VotingItemMapper;
 import com.dev.careers.mapper.VotingMapper;
 import com.dev.careers.model.Voting;
 import com.dev.careers.model.VotingItem;
+import com.dev.careers.service.error.AuthorMismatchException;
+import com.dev.careers.service.error.CursorOutOfRangeException;
+import com.dev.careers.service.error.FailToSaveVotingException;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import org.junit.Rule;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -71,14 +84,9 @@ public class VotingServiceTest {
         votings.add(voting);
 
         given(votingMapper.getTotalVotingCount()).willReturn(1);
-        given(votingMapper.getVotingList(1, 1)).willReturn(votings);
+        given(votingMapper.getVotingList(1, 10)).willReturn(votings);
 
         given(votingMapper.getVoting(1)).willReturn(Optional.of(voting));
-
-        willDoNothing().given(votingMapper).saveVoting(voting);
-
-        given(votingMapper.getVotingWriter(1)).willReturn(1);
-        willDoNothing().given(votingMapper).removeVoting(1);
     }
 
     public void mockVotingItemMapper() {
@@ -92,15 +100,11 @@ public class VotingServiceTest {
         votingItems.add(votingItem);
 
         given(votingItemMapper.getVotingItemList(1)).willReturn(votingItems);
-
-        willDoNothing().given(votingItemMapper).saveVotingItems(votingItems);
-
-        willDoNothing().given(votingItemMapper).removeVotingItems(1);
     }
 
     @Test
     @DisplayName("정상적인 투표 목록 조회 처리")
-    public void getVotings_ValidData_True() throws Exception {
+    public void getVotings_ValidData_True() {
         List<Voting> votings = votingService.getVotings(1);
         Voting voting = votings.get(0);
 
@@ -108,8 +112,20 @@ public class VotingServiceTest {
     }
 
     @Test
+    @DisplayName("cursor가 현재 저장된 투표수보다 높은 숫자인 경우 투표 목록 조회 처리")
+    public void getVotings_InvalidData_ExceptionThrown() {
+        int cursor = 2;
+        CursorOutOfRangeException exception = assertThrows(CursorOutOfRangeException.class,
+            () -> votingService.getVotings(cursor));
+
+        String message = exception.getMessage();
+        assertEquals("투표 목록 조회 범위를 초과하였습니다.", message);
+        verify(votingMapper, times(0)).getVotingList(anyInt(), anyInt());
+    }
+
+    @Test
     @DisplayName("정상적인 투표 상세 조회 처리")
-    public void getVoting_ValidData_True() throws Exception {
+    public void getVoting_ValidData_True() {
         Voting voting = votingService.getVoting(1);
 
         verify(votingItemMapper).getVotingItemList(eq(1));
@@ -124,7 +140,7 @@ public class VotingServiceTest {
 
     @Test
     @DisplayName("정상적인 데이터 형식 투표 생성 처리")
-    public void addVoting_ValidData_True() throws Exception {
+    public void addVoting_ValidData_True() {
         List<VotingItem> votingItems = new ArrayList<VotingItem>();
         LocalDateTime localDateTime = LocalDateTime.now();
         Timestamp timestamp = Timestamp.valueOf(localDateTime);
@@ -147,6 +163,9 @@ public class VotingServiceTest {
             .votingItems(votingItems)
             .build();
 
+        willDoNothing().given(votingMapper).saveVoting(voting);
+        willDoNothing().given(votingItemMapper).saveVotingItems(votingItems);
+
         votingService.addVoting(voting);
 
         verify(votingMapper).saveVoting(any(Voting.class));
@@ -155,8 +174,49 @@ public class VotingServiceTest {
     }
 
     @Test
+    @DisplayName("cursor가 현재 저장된 투표수보다 높은 숫자인 경우 투표 목록 조회 처리")
+    public void addVoting_InvalidData_ExceptionThrown() {
+        List<VotingItem> votingItems = new ArrayList<VotingItem>();
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Timestamp timestamp = Timestamp.valueOf(localDateTime);
+        Timestamp deadline = Timestamp.valueOf(localDateTime.plusDays(7));
+        VotingItem votingItem = VotingItem.builder()
+            .votingItemId(1)
+            .votingId(1)
+            .votingItemName("testItem")
+            .voteCount(1)
+            .build();
+        votingItems.add(votingItem);
+
+        Voting voting = Voting.builder()
+            .votingTitle("Test")
+            .votingWriter(1)
+            .timestamp(timestamp)
+            .deadline(deadline)
+            .votingExplanation("getVoting_Test")
+            .votingItems(votingItems)
+            .build();
+
+       willThrow(new FailToSaveVotingException("투표를 저장할 수 없습니다."))
+           .given(votingMapper)
+           .saveVoting(voting);
+
+       FailToSaveVotingException exception = assertThrows(FailToSaveVotingException.class,
+           ()->votingMapper.saveVoting(voting));
+
+       assertEquals("투표를 저장할 수 없습니다.", exception.getMessage());
+        verify(votingMapper, times(1)).saveVoting(any(Voting.class));
+        verify(votingItemMapper, times(0)).saveVotingItems(anyList());
+    }
+
+
+    @Test
     @DisplayName("정상적인 투표 제거 처리")
-    public void deleteVoting_ValidData_True() throws Exception {
+    public void deleteVoting_ValidData_True() {
+        given(votingMapper.getVotingWriter(1)).willReturn(1);
+        willDoNothing().given(votingMapper).removeVoting(1);
+        willDoNothing().given(votingItemMapper).removeVotingItems(1);
+
         votingService.deleteVoting(1,1);
 
         assertThat(votingMapper.getVotingWriter(1), is(1));
@@ -164,6 +224,21 @@ public class VotingServiceTest {
         verify(votingMapper).removeVoting(anyInt());
 
         verify(votingItemMapper).removeVotingItems(anyInt());
+    }
+
+    @Test
+    @DisplayName("삭제할 투표 작성자와 삭제 요청자가 일치하지 않을 경우에 처리")
+    public void deleteVoting_InvalidData_ExceptionThrows() {
+        AuthorMismatchException exception = assertThrows(AuthorMismatchException.class,
+            () -> votingService.deleteVoting(1, 1));
+
+        String message = exception.getMessage();
+
+        assertEquals("해당 요청자와 투표 작성자는 일치하지 않습니다.", message);
+
+        verify(votingMapper, times(0)).removeVoting(anyInt());
+
+        verify(votingItemMapper, times(0)).removeVotingItems(anyInt());
     }
 
 }
